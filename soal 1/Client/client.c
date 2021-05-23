533fb74174651b4cea5907de8e914139aaf186f6
DicksenAN <65166398+DicksenAN@users.noreply.github.com>
2021-05-23 18:35:29 +0700

    Update client.c
    Hasil Revisi, telah benar

--- soal 1/Client/client.c
+++ soal 1/Client/client.c
@@ -3,12 +3,25 @@
 #include <sys/socket.h>	
 #include <arpa/inet.h>	
 #include <unistd.h>
+#include <stdlib.h>
+
+void menu_utama(); //Fungsi menunjukkan menu utama setelah berhasil login
+void add_file(); //Fungsi menambah file kedalam Direktori FILES
+void download_file(); //Fungsi mengunduh file dari server
+void hapus_file();//Fungsi menghapus file dari server
+void lihat_file();//Fungsi melihat file yang ada di server
+void cari_file();//Fungsi mencari file yang ada di server
+
+int sock;
+struct sockaddr_in server;
+char message[1000] , server_reply[2000];
+int start;
+char clReply[1000];//Menerima perintah
+char clSend[2000];//Perintah yang dikirim
+char currentUser[200]; //Pengguna sekarang
 
 int main(int argc , char *argv[])
 {
-	int sock;
-	struct sockaddr_in server;
-	char message[1000] , server_reply[2000];
 	
 	sock = socket(AF_INET , SOCK_STREAM , 0);
 	if (sock == -1)
@@ -28,84 +41,224 @@ int main(int argc , char *argv[])
 	}
 	
 	puts("Connected\n");
-	int start = 0;
 	while(1)
 	{
-		if(start ==0){
-		if( recv(sock , server_reply , 2000 , 0) < 0)
-		{
-			puts("recv failed");
-			break;
-		}
-		puts(server_reply);
+        //first time receive message from server
+        // parsing pesan server
+        memset(server_reply, 0, sizeof(server_reply));
+	    read(sock, server_reply, 1024);
+        printf("SERVER NGIRIM = %s\n" , server_reply);
+        if (strcmp(server_reply, "menu") == 0) {
+            printf("Welcome, please select a command :\n1.Register\n2.Login\n");
+            printf("Menu chosen:\n");
+            scanf("%s", clReply);
 
-		printf("Enter message : ");
-		scanf("%s" , message);
-		
-		//Send some data
-		if( send(sock , message , strlen(message) , 0) < 0)
-		{
-			puts("Send failed");
-			return 1;
-		}
-		}
-		start=1;
-		printf("Enter message : ");
-		scanf("%s" , message);
-		
-			/*if( send(sock , username , strlen(username) , 0) < 0)
-		    {
-			puts("Send failed");
-			return 1;
+            //concate op1 atau op2
+            snprintf(clSend, sizeof clSend, "op%s", clReply);
+            
+            //kirim ke server
+            if( send(sock , clSend , strlen(clSend) , 0) < 0){
+			    puts("Send failed");
+			    return 1;
 		    }
-            if( send(sock , password , strlen(password) , 0) < 0)
-		    {
-			puts("Send failed");
-			return 1;
-		    }*/
-		char *register_user, *login_user;
-        register_user = "Please type username & password :\n";
-		login_user = "Please login using username & password:\n";
-        //puts("Server reply :");
-		//Receive a reply from the server
-		if( recv(sock , server_reply , 2000 , 0) < 0)
-		{
-			puts("recv failed");
-			break;
-		}
-		puts(server_reply);
-        if(strcmp(server_reply,register_user) == 0 || strcmp(server_reply,login_user) == 0){
-            printf("Username : ");
-			char username[2000];
-			char password[2000];
-            char username_1[200];
-            char password_1[200];
-			char Er[] = "R";
-			char Pe[] = "P";
-            scanf("%s", username_1);
-            printf("Password : ");
-            scanf("%s", password_1);
-			strcat(username, Er);
-			strcat(username,username_1);
-			strcat(password, Pe);
-			strcat(password,password_1);
-			printf("%s %s\n",username, password);
-			//snprintf(username, sizeof username, "%c%s",  Er,username_1);
-			//snprintf(password, sizeof password, "%c%s", Pe,password_1);
-            if( send(sock , username , strlen(username) , 0) < 0)
-		    {
-			puts("Send failed");
-			return 1;
-		    }
-            if( send(sock , password , strlen(password) , 0) < 0)
-		    {
-			puts("Send failed");
-			return 1;
+            start =1;
+            memset(clSend, 0,sizeof(clSend));
+            memset(clReply, 0, sizeof(clReply));
+        } else if (strcmp(server_reply, "register") == 0) {
+            char clUsername[100]; char clPassword[100]; char clAkun[300]; 
+            printf("Username:");
+            getchar();
+            scanf("%[^\n]s", clUsername);
+            printf("Password:");
+            getchar();
+            scanf("%[^\n]s", clPassword);
+            
+            //combine reg user:paaaaass 
+            snprintf(clAkun, sizeof clAkun, "R%s:%s", clUsername, clPassword);
+
+            //kirim ke server
+            if( send(sock , clAkun , strlen(clAkun) , 0) < 0){
+			    puts("Send failed");
+			    return 1;
 		    }
 
+        }else if (strcmp(server_reply, "login") == 0) {
+            char clUsername[100]; char clPassword[100]; char clAkun[300]; 
+            printf("Username:");
+            getchar();
+            scanf("%[^\n]s", clUsername);
+            printf("Password:");
+            getchar();
+            scanf("%[^\n]s", clPassword);
+            
+            //combine reg user:paaaaass 
+            snprintf(clAkun, sizeof clAkun, "L%s:%s", clUsername, clPassword);
+            //clReply=strcat("op"+clReply);
+            //printf("%s\n", clAkun);
+
+            //kirim ke server
+            if( send(sock , clAkun , strlen(clAkun) , 0) < 0){
+			    puts("Send failed");
+			    return 1;
+		    }
+
+        } else if (strcmp(server_reply, "Maaf") == 0){
+            printf("Welcome, please select a command :\n1.Register\n2.Login\n");
+            printf("Menu chosen:\n");
+            scanf("%s", clReply);
+
+            //concate op1 atau op2
+            snprintf(clSend, sizeof clSend, "op%s", clReply);
+            
+            //kirim ke server
+            if( send(sock , clSend , strlen(clSend) , 0) < 0){
+			    puts("Send failed");
+			    return 1;
+		    }
+            memset(clSend, 0,sizeof(clSend));
+            memset(clReply, 0, sizeof(clReply));
+        } else if (strcmp(server_reply, "mainMenu") == 0){
+            menu_utama();
+        } else if(strcmp(server_reply, "mohon tunggu") == 0){
+            puts(server_reply);
+        } else{
+            puts(server_reply);
+            printf("Maaf ada yang salah\n");
+            exit(1);
         }
-	}
+
+
+    memset(clSend, 0,sizeof(clSend));
+    memset(clReply, 0, sizeof(clReply));
+    }
+		
 	
 	close(sock);
 	return 0;
+}
+
+void menu_utama(){
+    memset(clSend, 0,sizeof(clSend));
+    printf("Hello please select a command :\n1.Add\n2.Download\n3.Delete\n4.See\n5.find\n");
+    printf("Menu chosen : ");
+    scanf("%s", clReply);
+    const char* perintah;
+    if(clReply[0] == '1'){
+        add_file();
+    }else if(clReply[0] == '2'){
+        download_file();
+    }else if(clReply[0] == '3'){
+        hapus_file();
+    }else if(clReply[0] == '4'){
+        lihat_file();
+    }else if(clReply[0] == '5'){
+        cari_file();
+    }
+    
+}
+
+
+void add_file(){
+    char publisher[200];
+    char tahun_publikasi[20];
+    char filepath[200];
+    char kirim_add_file[700];
+    printf("Publisher:");
+    getchar();
+    scanf("%[^\n]s", publisher);
+    printf("Tahun Publikasi:");
+    getchar();
+    scanf("%[^\n]s", tahun_publikasi);
+    printf("Filepath:");
+    getchar();
+    scanf("%[^\n]s", filepath);
+    //concate perintah
+    snprintf(clSend, sizeof clSend, "M1%s--%s--%s", publisher, tahun_publikasi, filepath);        
+    printf("%s\n",clSend);
+
+     if( send(sock , clSend , strlen(clSend) , 0) < 0){
+	    puts("Send failed");
+		return;
+		}
+}
+
+void download_file(){
+    char nama_file[200];
+    printf("download : ");
+    getchar();
+    scanf("%[^\n]s", nama_file);
+    //concate perintah
+    snprintf(clSend, sizeof clSend, "M2%s", nama_file);
+    
+    if( send(sock , clSend , strlen(clSend) , 0) < 0){
+		puts("Send failed");
+		return ;
+	}
+
+    read(sock, server_reply, 1024);
+    if(strcmp (server_reply, "noFile") == 0){
+        printf("Maaf file tidak ada dalam database\n");
+    }
+    else{
+        printf("Berhasil di download\n");
+        menu_utama();
+    }
+}
+
+void hapus_file(){
+    char nama_file[200];
+    printf("delete : ");
+    getchar();
+    scanf("%[^\n]s", nama_file);
+    //concate perintah
+    snprintf(clSend, sizeof clSend, "M3%s", nama_file);
+    if( send(sock , clSend , strlen(clSend) , 0) < 0){
+		puts("Send failed");
+		return ;
+	}
+    read(sock, server_reply, 1024);
+    if(strcmp (server_reply, "noFile") == 0){
+        printf("Maaf file tidak ada dalam database\n");
+        menu_utama();
+    }
+    else if (strcmp(server_reply, "mainMenu") == 0){
+        printf("Berhasil di hapus\n");
+        menu_utama();
+    }
+}
+
+void lihat_file(){
+    if( send(sock , "M4" , 2 , 0) < 0){
+		puts("Send failed");
+		return ;
+	}
+    memset(server_reply, 0, sizeof(server_reply));
+    read(sock, server_reply, 1024);
+    while(strcmp(server_reply, "mainMenu") != 0){
+        printf("%s\n", server_reply);
+        memset(server_reply, 0 , sizeof (server_reply));
+        read(sock, server_reply, 1024);
+    }
+    menu_utama();
+}
+
+void cari_file(){
+    char nama_file[200];
+    printf("find : ");
+    getchar();
+    scanf("%[^\n]s", nama_file);
+    //concate perintah
+    snprintf(clSend, sizeof clSend, "M5%s", nama_file);
+    if( send(sock , clSend , strlen(clSend) , 0) < 0){
+		puts("Send failed");
+		return ;
+	}
+    memset(server_reply, 0, sizeof(server_reply));
+    read(sock, server_reply, 1024);
+    while(strcmp(server_reply, "mainMenu") != 0){
+        printf("%s\n", server_reply);
+        memset(server_reply, 0 , sizeof (server_reply));
+        read(sock, server_reply, 1024);
+    }
+    menu_utama();
 }
